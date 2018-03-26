03dc21965761f0a664a2e29e93378fbe6f07e537
theDub <amarnaso@ualberta.ca>
2018-03-26 02:31:05 -0600

    Added some visual stuff, a cool circle for vertices. Cleaned up logic, and extracted into other functions. Added some constants.

--- draw.js
+++ draw.js
@@ -1,68 +1,131 @@
-var points = [];
-
-
+// run this when dom has loaded.
 window.onload = function() {
-    console.log('test');
-    draw();
+    console.log('Welcome to drywall calculator');
+    start();
 }
 
+// Good ol' globals
+const UNIT = 50;
+const POINT_OUTER = 8;
+const POINT_INNER = 4;
+const LINE_WIDTH = 5;
+const SCREEN_WIDTH = 1900;
+const SCREEN_HEIGHT = 1100;
+var points = [];
+var stored_points = [];
 
-function draw() {
+// Log function display route
+var log = function(){
+    console.log("Path Ended...");
+    stored_points.forEach(function(ele){
+        console.log("(x,y) => (" + ele.x + ",\t" + ele.y + ")");
+    });
+};
+
+
+// draws single line
+var drawLine = function(p1, p2, styling, thickness) {
     var canvas = document.getElementById('blueprint'),
         context = canvas.getContext('2d');
-    var count = 0;
-    var unit = 50;
 
-    var xDelta = unit;
-    var yDelta = unit;
+    context.beginPath();
+    context.lineWidth=thickness;
+    context.strokeStyle=styling;
+    context.moveTo(p1.x, p1.y);
+    context.lineTo(p2.x, p2.y);
+    context.stroke();
+};
 
-    canvas.addEventListener('click', function(event){
+
+// draws dot
+var drawDot = function(p, styling, radius) {
+    var canvas = document.getElementById('blueprint'),
+        context = canvas.getContext('2d');
+
+    context.beginPath();
+    context.strokeStyle=styling;
+    context.arc(p.x, p.y, radius, 0, 2 * Math.PI);
+    context.fill();
+    context.stroke();
+};
+
+// called in main logic, handles what to draw
+var draw = function(p1, p2) {
+    var canvas = document.getElementById('blueprint'),
+        context = canvas.getContext('2d');
+    if (points.length == 2){
+        drawDot(p2, "#d3d3d3", POINT_OUTER);
+        drawLine(p1, p2, "#000000", LINE_WIDTH);
+        drawDot(p2, "#000000", POINT_INNER);
+        points.shift();
+    } else {
+        drawDot(p1, "#d3d3d3", POINT_OUTER);
+        drawDot(p1, "#000000", POINT_INNER);
+    }
+};
+
+
+var sectionComplete = function() {
+        if (stored_points.length >= 2) {
+            if (stored_points[stored_points.length-2].x == stored_points[stored_points.length-1].x) {
+                if (stored_points[stored_points.length-2].y == stored_points[stored_points.length-1].y) {
+                    return true;
+                }
+            }
+        }
+        return false;
+};
+
+
+// initialize the grid in the background.
+var initialize = function() {
+    var canvas = document.getElementById('blueprint'),
+        context = canvas.getContext('2d');
+    var xDelta = UNIT;
+    var yDelta = UNIT;
+
+    // DRAW LINES
+    do {
+        drawLine({ x: xDelta, y: UNIT},
+                 { x: xDelta, y: SCREEN_HEIGHT - UNIT},
+                 "#d3d3d3",
+                 LINE_WIDTH
+        );
+        xDelta += UNIT;
+    } while (xDelta < SCREEN_WIDTH);
+
+    //horizontal lines
+    do {
+        drawLine({ x: UNIT, y: yDelta},
+                 { x: SCREEN_WIDTH - UNIT, y: yDelta},
+                 "#d3d3d3",
+                 LINE_WIDTH
+        );
+        yDelta += UNIT;
+    } while(yDelta < 1080);
+};
+
+
+function start() {
+
+
+    document.getElementById('blueprint').addEventListener('click', function(event){
+
         var point = {
-            x: Math.round(event.clientX/unit) * unit,
-            y: Math.round(event.clientY/unit) * unit
+            x: Math.round(event.clientX/UNIT) * UNIT,
+            y: Math.round(event.clientY/UNIT) * UNIT
         };
 
-        points.push(point);
-        console.log(points);
-        if (points.length == 2){
-            context.beginPath();
-            context.lineWidth="5";
-            context.strokeStyle="#000000";
-            context.moveTo(points[0].x, points[0].y);
-            context.lineTo(points[1].x, points[1].y);
-            context.stroke();
-            context.rect(points[1].x, points[1].y, 5, 5);
-            context.fill();
-            points.shift();
+        stored_points.push(point);
+        if (sectionComplete()) {
+            log()
+            stored_points = [];
+            points = [];
         } else {
-            context.rect(points[0].x, points[0].y, 5, 5);
-            context.fill();
+            points.push(point);
+            draw(points[0], points[1]);
         }
     }, false);
 
-    //vertical lines
-    do {
-        context.beginPath();
-        context.lineWidth="5";
-        context.strokeStyle="#d3d3d3";
-        context.moveTo(xDelta, unit);
-        context.lineTo(xDelta, 1100-unit);
-        context.stroke();
-        xDelta += unit;
-        count += 1;
-    } while (xDelta < 1900);
-
-    var verticalLines = count;
-    count = 0;
-    //horizontal lines
-    do {
-        context.beginPath();
-        context.lineWidth="5";
-        context.strokeStyle="#d3d3d3";
-        context.moveTo(unit, yDelta);
-        context.lineTo(1900-unit, yDelta);
-        context.stroke();
-        yDelta += unit;
-    } while(yDelta < 1080);
-        count += 1;
+    initialize();
 }
